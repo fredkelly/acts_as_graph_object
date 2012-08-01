768b3d9f9cdd954263a584a7938c0777788ba987
Fred Kelly <me@fredkelly.net>
2012-08-01 22:17:07 +0100

    Moved configuration into lib/configuration.rb

--- lib/acts_as_graph_object.rb
+++ lib/acts_as_graph_object.rb
@@ -1,28 +1,4 @@
+require "acts_as_graph_object/configuration"
 require "acts_as_graph_object/base"
 require "acts_as_graph_object/railite"
 require "acts_as_graph_object/version"
-
-module ActsAsGraphObject
-  # Configures ActsAsGraphObject.
-  def self.configure(configuration = ActsAsGraphObject::Configuration.new)
-    yield configuration if block_given?
-    @@configuration = configuration
-  end
-      
-  def self.configuration # :nodoc:
-    @@configuration ||= ActsAsGraphObject::Configuration.new
-  end
-
-  # Can be configured using the ActsAsGraphObject.configure method. For example:
-  # 
-  # ActsAsGraphObject.configure do |config|
-  #   config.namespace = 'my-app'
-  # end
-  class Configuration
-    attr_accessor :namespace, :app_id, :admins
-
-    def initialize # :nodoc:
-      # set any defaults in here
-    end
-  end
-end
---
+++ lib/acts_as_graph_object/configuration.rb
@@ -0,0 +1,24 @@
+module ActsAsGraphObject
+  # Configures ActsAsGraphObject.
+  def self.configure(configuration = ActsAsGraphObject::Configuration.new)
+    yield configuration if block_given?
+    @@configuration = configuration
+  end
+      
+  def self.configuration # :nodoc:
+    @@configuration ||= ActsAsGraphObject::Configuration.new
+  end
+
+  # Can be configured using the ActsAsGraphObject.configure method. For example:
+  # 
+  # ActsAsGraphObject.configure do |config|
+  #   config.namespace = 'my-app'
+  # end
+  class Configuration
+    attr_accessor :namespace, :app_id, :admins
+
+    def initialize # :nodoc:
+      # set any defaults in here
+    end
+  end
+end
