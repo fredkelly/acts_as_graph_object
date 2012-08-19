0af255762bdf8ac71e58223bc8f1d2a930299577
Fred Kelly <me@fredkelly.net>
2012-08-19 19:02:26 +0100

    Fixed some errors in helper.rb

--- lib/acts_as_graph_object/helpers.rb
+++ lib/acts_as_graph_object/helpers.rb
@@ -1,11 +1,11 @@
 module ActsAsGraphObject
   module ViewHelpers
-    def graph_object_tags_for(object, options = {})
+    def graph_object_tags_for(object, overrides = {})
       raise "You need to add acts_as_graph_object to your #{object.class} model." unless object.respond_to?(:graph_properties)
       meta_tags = []
-      object.graph_properties.each do |namespace, attributes|
-        # merge to override/set from view
-        attributes.merge(options).each do |property, values|
+      # go through each namespace: :fb, :og & custom..
+      object.graph_properties.deep_merge(:og => overrides).each do |namespace, attributes|
+        attributes.each do |property, values|
           # in most cases 'values' will be a single value
           Array(values).each do |content|
             meta_tags << tag(:meta, :property => [namespace, property].join(':'), :content => content)
